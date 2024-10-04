75475c43a093be63f625f5940dcdcab8783ee36e
Dwehsidi <42282793+Dwehsidi@users.noreply.github.com>
2024-10-03 21:58:46 -0500

    Remove node_modules and update .gitignore

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,9 @@
 import { Fragment } from "react";
 import HeroSection from "../../components/home/HeroSection";
-import HowToSection from "../../components/home/HowToSection";
 import DeveloperSection from "../../components/home/DeveloperSection";
-import RoadmapSection from "../../components/home/RoadmapSection";
 import FeaturesSection from "../../components/home/FeaturesSection";
 import WhyUsSection from "../../components/home/WhyUsSection";
 import FaqSection from "../../components/home/FaqSection";
-import TeamSection from "../../components/home/TeamSection";
 import NetworkSection from "../../components/home/NetworkSection";
 import MainNavigation from "../../components/layout/Header/MainNavigation";
 import Footer from "../../components/layout/Footer";
@@ -18,9 +15,6 @@ export default function Home() {
             <HeroSection />
             <FeaturesSection />
             <WhyUsSection />
-            {/* <HowToSection />
-            <TokenomicsSection /> 
-            <RoadmapSection />*/}
             <DeveloperSection />
             <NetworkSection />
             <FaqSection />
